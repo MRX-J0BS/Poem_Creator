ebf7b2bfc208cbf80c38a630f59467eedf36f775
MRX <110516865+MRX-J0BS@users.noreply.github.com>
2022-12-16 21:48:32 +0800

    Add files via upload
    新增：唯一标识码生成

--- PCear_MainFunction.py
+++ PCear_MainFunction.py
@@ -7,16 +7,13 @@ charList=[]
 for i in range(0x4e00,0x9fa6):
     charList.append(chr(i))
 
-#print(charList)
-shuffle(charList)
-#print(charList)
 
 
 def makeALetter(num,time):
 #num 每行有几个字  time 有几行
     #num
     tempLetter=[] # 一个局部列表，用于临时存储抽取出来的字符
-
+    onlyKeyList=[]
     # 用于计算所有的可能性
     x=len(charList)
     possible=str(math.pow(x,time))
@@ -44,5 +41,26 @@ def makeALetter(num,time):
             ltime=0
     print(output)
 
+
+
+
+    #唯一标志
+    for w in tempLetter:
+        for q in range(0,len(charList)):
+            if w == charList[q]:
+                onlyKeyList.append(q)
+    onlyKey=''
+    for x in onlyKeyList:
+        onlyKey=onlyKey+str(x)+'|'
+
+    print(onlyKey)
+
+
+
+
+
 makeALetter(5,4)
+
+
+
 
